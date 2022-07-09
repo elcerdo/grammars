a98af02821d1257979190bbacbc83222ab4f5ad8
Pierre Gueth <gueth@adobe.com>
2022-07-09 18:09:23 +0200

    actually testing lemon works

--- calc/grammars/tests/test_exprtree.cpp
+++ calc/grammars/tests/test_exprtree.cpp
@@ -7,4 +7,20 @@
 TEST_CASE("test lemon", "[lemon]")
 {
   using Graph = lemon::ListDigraph;
+
+  Graph graph;
+  const auto na = graph.addNode();
+
+  REQUIRE(lemon::countNodes(graph) == 1);
+  REQUIRE(lemon::countArcs(graph) == 0);
+
+  const auto nb = graph.addNode();
+
+  REQUIRE(lemon::countNodes(graph) == 2);
+  REQUIRE(lemon::countArcs(graph) == 0);
+
+  const auto e0 = graph.addArc(na, nb);
+
+  REQUIRE(lemon::countNodes(graph) == 2);
+  REQUIRE(lemon::countArcs(graph) == 1);
 }
