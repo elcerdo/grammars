5abbaf19aafc84bb37cf91e82414bb6ff0ae1500
Pierre Gueth <gueth@adobe.com>
2022-07-23 13:44:07 +0100

    linux fix

--- calc/cmake/bison.cmake
+++ calc/cmake/bison.cmake
@@ -1,11 +1,13 @@
 
 message(STATUS "Getting bison release")
 
+if (MSVC)
 FetchContent_Declare(winbison
   URL https://github.com/lexxmark/winflexbison/releases/download/v2.5.25/win_flex_bison-2.5.25.zip
   URL_HASH SHA256=8d324b62be33604b2c45ad1dd34ab93d722534448f55a16ca7292de32b6ac135)
 FetchContent_Populate(winbison)
 
 set(BISON_EXECUTABLE "${winbison_SOURCE_DIR}/win_bison.exe" CACHE FILEPATH "" FORCE)
+endif()
 
 find_package(BISON 3.2)
